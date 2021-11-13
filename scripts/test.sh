6e9c0a8629b3199a3150c130d788196b6924653f
Sean McCarthy <sean.mccarthy@hotmail.com>
2021-11-13 08:10:21 -0700

    re-added typer-cli and made test script work

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-pytest ../tests/ --cov=responsive_images_generator --cov=tests --cov-report=term-missing ${@}
-bash ./lint.sh
+pytest ./tests/ --cov=responsive_images_generator --cov=tests --cov-report=term-missing ${@}
+bash ./scripts/lint.sh
