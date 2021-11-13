ca0052e90d07eef49a2ce0c61d5bef88d3cc7c2e
Sean McCarthy <sean.mccarthy@hotmail.com>
2021-11-13 08:15:10 -0700

    Set the current working directory to the directory in which the script is located, for CI/CD

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,11 +3,14 @@
 set -e
 set -x
 
+# Set the current working directory to the directory in which the script is located, for CI/CD
+cd "$(dirname "$0")"
+
 # Remove unused imports and unused variables
-autoflake --in-place --remove-unused-variables --remove-all-unused-imports --verbose --recursive /workspace/responsive_images_generator/
+autoflake --in-place --remove-unused-variables --remove-all-unused-imports --verbose --recursive ../responsive_images_generator/
 
 # Nice sorting of imports
-isort /workspace/responsive_images_generator/
+isort ../responsive_images_generator
 
 # Opinionated but lovely auto-formatting
-black /workspace/responsive_images_generator
+black ../responsive_images_generator
