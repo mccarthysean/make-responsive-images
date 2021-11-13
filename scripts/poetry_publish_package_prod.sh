d6710dfda27bb69dd4e9871176a41806e1c90e43
Sean McCarthy <sean.mccarthy@hotmail.com>
2021-11-13 08:51:48 -0700

    testing whether I need username and password for PyPI

--- scripts/poetry_publish_package_prod.sh
+++ scripts/poetry_publish_package_prod.sh
@@ -9,4 +9,5 @@ echo "Current working directory: $(pwd)"
 # https://python-poetry.org/docs/libraries/
 # https://python-poetry.org/docs/cli/#publish
 poetry config pypi-token.pypi $PYPI_TOKEN_PROD
-poetry publish --build --username $PYPI_USERNAME_PROD --password $PYPI_PASSWORD_PROD
+poetry publish --build 
+# poetry publish --build --username $PYPI_USERNAME_PROD --password $PYPI_PASSWORD_PROD
