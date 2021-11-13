443662ccee21ac0929f2537a33d45ba687836238
Sean McCarthy <sean.mccarthy@hotmail.com>
2021-11-13 08:38:44 -0700

    updated PyPI test publish

--- scripts/poetry_publish_package_test.sh
+++ scripts/poetry_publish_package_test.sh
@@ -8,10 +8,10 @@ echo "Current working directory: $(pwd)"
 
 # https://python-poetry.org/docs/libraries/
 # https://python-poetry.org/docs/cli/#publish
-# poetry config repositories.testpypi https://test.pypi.org/legacy/
-# poetry config pypi-token.pypi $PYPI_TOKEN_TEST
-poetry publish --build --repository testpypi
-# poetry publish --build --repository testpypi --username $PYPI_USERNAME_TEST --password $PYPI_PASSWORD_TEST
+poetry config repositories.testpypi https://test.pypi.org/legacy/
+poetry config pypi-token.pypi $PYPI_TOKEN_TEST
+# poetry publish --build --repository testpypi
+poetry publish --build --repository testpypi --username $PYPI_USERNAME_TEST --password $PYPI_PASSWORD_TEST
 
 # Test that it worked
 # pip install --index-url https://test.pypi.org/simple/ responsive-images-generator
