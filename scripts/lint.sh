5605e936b9c192f9bf1ab63c820ee1743ffd2360
Sean McCarthy <sean.mccarthy@hotmail.com>
2021-11-14 11:23:30 -0700

    improved linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,32 +5,32 @@ set -x
 
 # Set the current working directory to the directory in which the script is located, for CI/CD
 cd "$(dirname "$0")"
+cd ..
 echo "Current working directory: $(pwd)"
-
-# Remove unused imports and unused variables
-echo ""
-echo "Running autoflake..."
-autoflake --in-place --remove-unused-variables --remove-all-unused-imports --verbose --recursive ../make_responsive_images/ ../tests
-
-echo ""
-echo "Running flake8..."
-flake8 ../make_responsive_images ../tests
 
 # Nice sorting of imports
 echo ""
 echo "Running isort..."
-# isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --thirdparty ../make_responsive_images ../tests
-isort --profile black ../make_responsive_images ../tests
+isort --profile black ./make_responsive_images ./tests
 
 # Opinionated but lovely auto-formatting
 echo ""
 echo "Running black..."
-black ../make_responsive_images ../tests
+black ./make_responsive_images ./tests
+
+# Remove unused imports and unused variables
+echo ""
+echo "Running autoflake..."
+autoflake --in-place --remove-unused-variables --remove-all-unused-imports --verbose --recursive ./make_responsive_images/ ./tests
+
+echo ""
+echo "Running flake8..."
+flake8 ./make_responsive_images ./tests
 
 echo ""
 echo "Running bandit..."
-bandit -r "../make_responsive_images"
-bandit -r "../tests" --configfile "../.bandit_4_tests.yml"
+bandit -r "./make_responsive_images"
+bandit -r "./tests" --configfile "./.bandit_4_tests.yml"
 
 echo ""
 echo "Running safety..."
@@ -39,4 +39,4 @@ safety check
 # Run mypy
 echo ""
 echo "Running mypy..."
-mypy --config-file ../mypy.ini ../make_responsive_images --disallow-untyped-defs
+mypy --config-file ./mypy.ini ./make_responsive_images --disallow-untyped-defs
