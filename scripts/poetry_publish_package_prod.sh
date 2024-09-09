f599e3816bd02700b31fa017c570515c057694a6
Sean McCarthy <sean.mccarthy@hotmail.com>
2024-09-09 13:34:25 -0600

    removed comments

--- scripts/poetry_publish_package_prod.sh
+++ scripts/poetry_publish_package_prod.sh
@@ -21,17 +21,11 @@ echo "Current working directory: $(pwd)"
 export $(cat ../.env | sed 's/#.*//g' | xargs)
 
 # Check if the variables are empty before proceeding
-# echo "PYPI_TOKEN_PROD: $PYPI_TOKEN_PROD"
 check_var "$PYPI_TOKEN_PROD" "PYPI_TOKEN_PROD"
 
 # First build the files to be uploaded
 poetry build
 
 # Publish to the production repository
-# https://python-poetry.org/docs/libraries/
-# https://python-poetry.org/docs/cli/#publish
-# poetry config pypi-token.pypi $PYPI_TOKEN_PROD
-# poetry publish --build 
-# poetry publish --build --username $PYPI_USERNAME_PROD --password $PYPI_PASSWORD_PROD
 poetry publish --username __token__ --password $PYPI_TOKEN_PROD
 
