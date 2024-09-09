261aa20aa72cbeaeb97c1aac7aadd773225ebdcb
Sean McCarthy <sean.mccarthy@hotmail.com>
2024-09-09 12:15:00 -0600

    update poetry scripts

--- scripts/poetry_publish_package_prod.sh
+++ scripts/poetry_publish_package_prod.sh
@@ -6,8 +6,17 @@ set -e
 cd "$(dirname "$0")"
 echo "Current working directory: $(pwd)"
 
+# Load environment variables from dotenv / .env file in Bash, and remove comments
+export $(cat ../.env | sed 's/#.*//g' | xargs)
+echo "PYPI_TOKEN_PROD: $PYPI_TOKEN_PROD"
+
+# First build the files to be uploaded
+poetry build
+
+# Publish to the production repository
 # https://python-poetry.org/docs/libraries/
 # https://python-poetry.org/docs/cli/#publish
-poetry config pypi-token.pypi $PYPI_TOKEN_PROD
-poetry publish --build 
+# poetry config pypi-token.pypi $PYPI_TOKEN_PROD
+# poetry publish --build 
 # poetry publish --build --username $PYPI_USERNAME_PROD --password $PYPI_PASSWORD_PROD
+poetry publish --username __token__ --password $PYPI_TOKEN_PROD
--- scripts/poetry_publish_package_test.sh
+++ scripts/poetry_publish_package_test.sh
@@ -6,12 +6,22 @@ set -e
 cd "$(dirname "$0")"
 echo "Current working directory: $(pwd)"
 
+# Load environment variables from dotenv / .env file in Bash, and remove comments
+export $(cat ../.env | sed 's/#.*//g' | xargs)
+echo "PYPI_TOKEN_TEST: $PYPI_TOKEN_TEST"
+
 # https://python-poetry.org/docs/libraries/
 # https://python-poetry.org/docs/cli/#publish
 poetry config repositories.testpypi https://test.pypi.org/legacy/
-poetry config pypi-token.pypi $PYPI_TOKEN_TEST
-poetry publish --build --repository testpypi
-# poetry publish --build --repository testpypi --username $PYPI_USERNAME_TEST --password $PYPI_PASSWORD_TEST
+# poetry config pypi-token.pypi $PYPI_TOKEN_TEST
+
+# First build the files to be uploaded
+poetry build
+
+# Publish to the test repository
+# poetry publish --repository testpypi
+poetry publish --repository testpypi --username __token__ --password $PYPI_TOKEN_TEST
+# poetry publish --repository testpypi --username $PYPI_USERNAME_TEST --password $PYPI_PASSWORD_TEST
 
 # Test that it worked
 # pip install --index-url https://test.pypi.org/simple/ make-responsive-images
